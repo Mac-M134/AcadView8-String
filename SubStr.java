fd8de4a333da3a462209df5fecb20d5c8eed47c9
Himanshu <Himanshu.m134@hotmail.com>
2018-09-11 23:43:17 +0530

    Strings

---
+++ SubStr.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class SubStr{
+   public static void main(String args[]){
+      String string, sub;
+      int i, c, length; 
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter a string:");
+      string  = in.nextLine(); 
+      length = string.length(); 
+      System.out.println("Substrings are:"); 
+      for (c = 0; c < length; c++){
+         for(i = 1; i <= length - c; i++){
+            sub = string.substring(c, c+i);
+            System.out.println(sub);
+            
+         }
+      }
+   }
+}
